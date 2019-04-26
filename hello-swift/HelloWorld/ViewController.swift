464bd373860d1f7471ee29f5402ebb69557a4a1a
numa <aytdm251@gmail.com>
2019-04-26 18:40:29 +0900

    Add hello-swift

---
+++ hello-swift/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by numa on 2019/04/12.
+//  Copyright © 2019 numa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello world", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
---
+++ hello-swift/README.md
@@ -0,0 +1,4 @@
+# Swift, Hello World
+
+Imitation of [Beginning iOS 12 Programming with Swift](https://www.appcoda.com/learnswift/)
+
