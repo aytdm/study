1ad9b696f66177a440a5a80bd074fc76bb1c2926
numa <aytdm@protonmail.com>
2020-12-15 23:10:08 +0900

    Improve error message

--- 012_9cc/9cc.c
+++ 012_9cc/9cc.c
@@ -24,9 +24,24 @@ struct Token {
 // token of interest
 Token *token;
 
+char *user_input;
+
 void error(char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -42,14 +57,14 @@ bool consume(char op) {
 
 void expect(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op) {
-        error("It's not '%c'.", op);
+        error_at(token->str, "It's not '%c'.", op);
     }
     token = token->next;
 }
 
 int expect_number() {
     if (token->kind != TK_NUM) {
-        error("It's not number.");
+        error_at(token->str, "It's not number.");
     }
     int val = token->val;
     token = token->next;
@@ -68,7 +83,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
     return tok;
 }
 
-Token *tokenize(char *p) {
+Token *tokenize() {
+    char *p = user_input;
     Token head;
     head.next = NULL;
     Token *cur = &head;
@@ -91,7 +107,7 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        error("Can't tokenize");
+        error_at(p, "Expected a number");
     }
 
     new_token(TK_EOF, cur, p);
@@ -104,8 +120,10 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    user_input = argv[1];
+
     // tokenize
-    token = tokenize(argv[1]);
+    token = tokenize();
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
