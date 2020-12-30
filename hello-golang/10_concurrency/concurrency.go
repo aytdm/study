c81d634a861ffdde8da759b341472e1dc12da50f
numa <aytdm251@gmail.com>
2020-12-31 04:12:32 +0900

    Add chapter 10

---
+++ hello-golang/10_concurrency/concurrency.go
@@ -0,0 +1,29 @@
+package main
+
+type WebsiteChecker func(string) bool
+
+type result struct {
+  string
+  bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+  results := make(map[string]bool)
+  resultChannel := make(chan result)
+
+  for _, url := range urls {
+    go func(u string) {
+      // send statement
+      resultChannel <- result{u, wc(u)}
+    }(url)
+  }
+
+
+  for i := 0; i < len(urls); i++ {
+    // receive expression
+    result := <-resultChannel
+    results[result.string] = result.bool
+  }
+
+  return results
+}
